84a5b05176558a894ff7560868263314edce79cc
Yann-Armel <professional.gallie@gmail.com>
2024-06-07 21:30:30 +0000

    Ajout du premier fichier php

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+// Premier utilisateur
+$userName1 = 'Mickael Audrieu';
+$userEmail1 = 'mickael.audrieu@gmail.com';
+$userPassword1 = 'S3cr3t';
+$userAge1 = 34;
+
+// Deuxième utilisatrice
+$userName2 = 'Laurène Castor';
+$userEmail2 = 'laurene.castor@gmail.com';
+$userPassword2 = 'P4ssW0rD';
+$userAge2 = 28;
+
+$user1 = ['Mickael Audrieu', 'mickael.audrieu@gmail.com', 'S3cr3t', 34];
+$user2 = ['Laurène Castor', 'laurene.castor@gmail.com', 'P4ssW0rD', 28];
+$users = [$user1, $user2];
