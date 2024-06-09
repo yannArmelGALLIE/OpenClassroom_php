b9d651ca90088bff068a6d1a27e9d866d2c41d81
Yann-Armel <professional.gallie@gmail.com>
2024-06-09 17:58:09 +0000

    Lier les fichiers variables.php, fonctions.php, en-tête.php et pied_de_page.php avec require_once

--- page d'accueil.php
+++ page d'accueil.php
@@ -1,78 +1,9 @@
 <?php
-
-//Les différentes recettes
-$recipes = [
-    [
-        'title' => 'Cassoulet',
-        'recipe' => 'Etape 1 : des flageolets !',
-        'author' => 'mickael.andrieu@exemple.com',
-        'is_enabled' => true,
-    ],
-    [
-        'title' => 'Couscous',
-        'recipe' => 'Etape 1 : de la semoule',
-        'author' => 'mickael.andrieu@exemple.com',
-        'is_enabled' => false,
-    ],
-    [
-        'title' => 'Escalope milanaise',
-        'recipe' => 'Etape 1 : prenez une belle escalope',
-        'author' => 'mathieu.nebra@exemple.com',
-        'is_enabled' => true,
-    ],
-];
-
-//Les differents utilisateurs
-$users = [
-    [
-    'full_name' => 'Mickaël Andrieu',
-    'email' => 'mickael.andrieu@exemple.com',
-    'age' => 34,
-    ],
-    [
-    'full_name' => 'Mathieu Nebra',
-    'email' => 'mathieu.nebra@exemple.com',
-    'age' => 34,
-    ],
-    [
-    'full_name' => 'Laurène Castor',
-    'email' => 'laurene.castor@exemple.com',
-    'age' => 28,
-    ],
-    ];
-
+//Les différents variables
+require_once(__DIR__ . '/variables.php');
+require_once(__DIR__ . '/fonctions.php');
 //Les différentes fonctions
-/*Vérifier la validité d'une recette*/ 
-function isValidRecipe(array $recipe) : bool 
-{
-    if(array_key_exists('is_enabled', $recipe)) {
-        $isEnabled = $recipe['is_enabled'];
-    } else {
-        $isEnabled = false;
-    }
-    return $isEnabled;
-}
-/*Récupérer les recettes valides*/ 
-function getRecipes(array $recipes) : array
-{
-    $validRecipes = [];
-    
-    foreach($recipes as $recipe){
-        if (isValidRecipe($recipe)){
-            $validRecipes[] = $recipe;
-        }
-    }
-    return $validRecipes;
-}
-/*Afficher le nom de l'auteur*/
-function displayAuthor(string $authorEmail, array $users): string
-{
- foreach($users as $user){
-    if ($authorEmail === $user['email']){
-        return  $user['full_name'] . '(' . $user['age'] . 'ans)'; 
-    } 
- }   
-}
+
 
 ?>
 
@@ -86,6 +17,7 @@ function displayAuthor(string $authorEmail, array $users): string
 </head>
 
 <body>
+    <?php require_once(__DIR__ . '/en-tête.php') ?>
     <h1><strong>Liste des recettes de cuisine</strong></h1>
     <?php foreach(getRecipes($recipes) as $recipe) : ?>
     <article>
@@ -94,6 +26,8 @@ function displayAuthor(string $authorEmail, array $users): string
         <i><?php echo displayAuthor($recipe['author'], $users); ?></i>
     </article>
     <?php endforeach; ?>
+    <?php require_once(__DIR__ . '/pied_de_page.php') ?>
+
 </body>
 
 </html>
