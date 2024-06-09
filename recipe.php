e3fd271b8ba1f38b093bbfd2958c81a58d58453a
Yann-Armel <professional.gallie@gmail.com>
2024-06-09 14:07:30 +0000

    Modification du h1 et du titre de la page. Ajout de trois fonctions, une pour vérifier la validité d'une recette, une autre pour récupérer les recettes valides et une dernière pour afficher le nom des auteurs

--- recipe.php
+++ recipe.php
@@ -1,5 +1,6 @@
 <?php
 
+//Les différentes recettes
 $recipes = [
     [
         'title' => 'Cassoulet',
@@ -21,6 +22,57 @@ $recipes = [
     ],
 ];
 
+//Les differents utilisateurs
+$users = [
+    [
+    'full_name' => 'Mickaël Andrieu',
+    'email' => 'mickael.andrieu@exemple.com',
+    'age' => 34,
+    ],
+    [
+    'full_name' => 'Mathieu Nebra',
+    'email' => 'mathieu.nebra@exemple.com',
+    'age' => 34,
+    ],
+    [
+    'full_name' => 'Laurène Castor',
+    'email' => 'laurene.castor@exemple.com',
+    'age' => 28,
+    ],
+    ];
+
+//Les différentes fonctions
+/*Vérifier la validité d'une recette*/ 
+function isValidRecipe(array $recipe) : bool 
+{
+    if(array_key_exists('is_enabled', $recipe)) {
+        $isEnabled = $recipe['is_enabled'];
+    } else {
+        $isEnabled = false;
+    }
+    return $isEnabled;
+}
+/*Récupérer les recettes valides*/ 
+function getRecipes(array $recipes) : array
+{
+    $validRecipes = [];
+    
+    foreach($recipes as $recipe){
+        if (isValidRecipe($recipe)){
+            $validRecipes[] = $recipe;
+        }
+    }
+    return $validRecipes;
+}
+/*Afficher le nom de l'auteur*/
+function displayAuthor(string $authorEmail, array $users): string
+{
+ foreach($users as $user){
+    if ($authorEmail === $user['email']){
+        return  $user['full_name'] . '(' . $user['age'] . 'ans)'; 
+    } 
+ }   
+}
 
 ?>
 
@@ -30,19 +82,17 @@ $recipes = [
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Affichage des recettes</title>
+    <title>Recettes de cuisine</title>
 </head>
 
 <body>
-    <h1><strong>Affichage de recettes</strong></h1>
-    <?php foreach($recipes as $recipe) : ?>
-    <?php if(array_key_exists('is_enabled', $recipe) && $recipe['is_enabled']) : ?>
+    <h1><strong>Liste des recettes de cuisine</strong></h1>
+    <?php foreach(getRecipes($recipes) as $recipe) : ?>
     <article>
-        <h3><?php echo $recipe['title'] ?></h3>
-        <p><?php echo $recipe['recipe'] ?></p>
-        <i><?php echo $recipe['author'] ?></i>
+        <h3><?php echo $recipe['title']; ?></h3>
+        <p><?php echo $recipe['recipe']; ?></p>
+        <i><?php echo displayAuthor($recipe['author'], $users); ?></i>
     </article>
-    <?php endif; ?>
     <?php endforeach; ?>
 </body>
 
