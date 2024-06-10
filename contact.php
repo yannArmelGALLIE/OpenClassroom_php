2a3d133be43b5679601dbb17152870bdc2623a00
Yann-Armel <professional.gallie@gmail.com>
2024-06-10 01:54:18 +0000

    Ajout d'un formulaire

---
+++ contact.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form action="submit_contact.php" method="GET">
+        <div>
+            <label for="email">Email</label>
+            <input type="email" name="email" id="">
+        </div>
+        <div>
+            <label for="message">Message</label>
+            <textarea name="message" id=""></textarea>
+        </div>
+        <button type="submit">Envoyer</button>
+    </form>
+</body>
+
+</html>
