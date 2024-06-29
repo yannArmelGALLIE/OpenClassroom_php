b97402e061bb4b7b0ab819897be871a386324280
Yann-Armel <professional.gallie@gmail.com>
2024-06-29 21:01:30 +0000

    Ajout de l'attribut enctype=multipart/form-data à la balise <form>

--- contact.php
+++ contact.php
@@ -8,7 +8,7 @@
 </head>
 
 <body>
-    <form action="submit_contact.php" method="POST">
+    <form action="submit_contact.php" method="POST" enctype="multipart/form-data">
         <div>
             <label for="email">Email</label>
             <input type="email" name="email" id="">
@@ -16,6 +16,10 @@
         <div>
             <label for="message">Message</label>
             <textarea name="message" id=""></textarea>
+        </div>
+        <div>
+            <label for="screenshot">Votre capture d'écran</label>
+            <input type="file" name="screenshot" id="screenshot">
         </div>
         <button type="submit">Envoyer</button>
     </form>
