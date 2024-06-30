c6afa23d20b6e1a64cb2a9d77f577206f0cefbd4
Yann-Armel <professional.gallie@gmail.com>
2024-06-30 03:38:26 +0000

    Ajout des mots de passe à chaque utilisateurs

--- variables.php
+++ variables.php
@@ -26,15 +26,18 @@ $users = [
     'full_name' => 'Mickaël Andrieu',
     'email' => 'mickael.andrieu@exemple.com',
     'age' => 34,
+    'password'  => 'mathieu2.0',
     ],
     [
     'full_name' => 'Mathieu Nebra',
     'email' => 'mathieu.nebra@exemple.com',
     'age' => 34,
+    'password'  => 'mathieu.food',
     ],
     [
     'full_name' => 'Laurène Castor',
     'email' => 'laurene.castor@exemple.com',
     'age' => 28,
+    'password'  => 'laurène.82',
     ],
     ];
