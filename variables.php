4f07edd6acf1310e5baca5e6127c939cc6868dce
Yann-Armel <professional.gallie@gmail.com>
2024-07-01 00:31:53 +0000

    ajout de la clé user_id à chaque utilisateur

--- variables.php
+++ variables.php
@@ -26,18 +26,21 @@ $users = [
     'full_name' => 'Mickaël Andrieu',
     'email' => 'mickael.andrieu@exemple.com',
     'age' => 34,
-    'password'  => 'mathieu2.0',
+    'password'  => 'mickael2.0',
+    'user_id'=> 0,
     ],
     [
     'full_name' => 'Mathieu Nebra',
     'email' => 'mathieu.nebra@exemple.com',
     'age' => 34,
     'password'  => 'mathieu.food',
+    'user_id'=> 1,
     ],
     [
     'full_name' => 'Laurène Castor',
     'email' => 'laurene.castor@exemple.com',
     'age' => 28,
     'password'  => 'laurène.82',
+    'user_id'=> 2,
     ],
     ];
